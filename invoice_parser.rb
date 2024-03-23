a7c07c40a36533c2aabe53b9d374a161964c32cb
Mohammed Adem <mohjj46@email.com>
2024-03-23 17:33:39 -0500

    Add URL extractor and invoice parser scripts

---
+++ invoice_parser.rb
@@ -0,0 +1,20 @@
+# invoice_parser.rb
+def parse_invoices(invoice_data)
+    pattern = /^(\d{4}-\d{2}-\d{2}) - (\w+) - ([\w\s]+) - (\$\d+)$/
+    invoice_data.each_line do |line|
+      match_data = line.match(pattern)
+      if match_data
+        date, invoice_number, client, amount = match_data.captures
+        puts "Date: #{date}, Invoice Number: #{invoice_number}, Client: #{client}, Amount: #{amount}"
+      end
+    end
+  end
+  
+  invoice_entries = <<-INVOICES
+  2023-03-01 - INV001 - Acme Corp - $1000
+  2023-03-02 - INV002 - Beta LLC - $2050
+  2023-03-03 - INV003 - Gamma Inc - $3500
+  INVOICES
+  
+  puts parse_invoices(invoice_entries)
+  
